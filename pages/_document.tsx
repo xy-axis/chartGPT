e44511278b56f90550ba7ca60aa61c07e01284ee
xiexin12138 <xiexin344@163.com>
2023-09-20 15:46:33 +0800

    添加埋点

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/lasso.ico" />
+        <script defer data-domain="chart.gpthink.top" src="https://analytics.gptnb.xyz/js/script.js"></script>
       </Head>
       <link
         href="https://fonts.googleapis.com/css2?family=Caveat+Brush&display=swap"
